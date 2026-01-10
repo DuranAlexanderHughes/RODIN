a21acd2d04ca08fd41c52b20051074f41a844322
Hughes, Alexander CIV (USA) <duran.a.hughes.civ@us.navy.mil>
2026-01-09 16:54:42 -0800

    added dynamic model selection

--- backend/app/agent.py
+++ backend/app/agent.py
@@ -1,6 +1,17 @@
+from dotenv import load_dotenv
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
+from langchain.tools import tool, ToolRuntime
+from dataclasses import dataclass
+from langchain_openai import ChatOpenAI
+
+from typing import Annotated, Optional, List
+
+load_dotenv()
 
 SYSTEM_PROMPT = """You are an expert in the lore of the BioShock videogame series.
 
@@ -26,3 +37,19 @@ advanced_model = ChatOpenAI(model="gpt-4o")
 @tool
 def get_bioshock_lore(query: str) -> str:
     """Query the RAG to get additional context from the scraped and vectorized bioshock wiki for generating answer"""
+
+@wrap_model_call
+def dynamic_model_selection(request: ModelRequest, handler) -> ModelResponse:
+    """Choose model based on conversation complexity."""
+    # TODO: add way to keep seperate count per user and reset count after X amount of time has passed
+    message_count = len(request.state["messages"])
+
+    if message_count > 10:
+        # Use an advanced model for longer conversations
+        model = advanced_model
+    else:
+        # Otherwise stick to basic, cheaper model
+        model = basic_model
+
+    request.model = model
+    return handler(request)
